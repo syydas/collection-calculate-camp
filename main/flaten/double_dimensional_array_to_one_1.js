5bd1be5aa3edbb8a660bc5ff2fed8aecd6d2298e
Asuu <suqinzheng_0624@163.com>
2020-01-17 15:55:14 +0800

    [zhengsuqin]refactor: refactor the function in flaten file

--- main/flaten/double_dimensional_array_to_one_1.js
+++ main/flaten/double_dimensional_array_to_one_1.js
@@ -1,19 +1,7 @@
 'use strict';
 
 function double_to_one(collection) {
-  let res = [];
-  for (let item of collection) {
-    if (item instanceof Array) {
-      for (let itemIn of item) {
-        res.push(itemIn);
-      }
-    }
-    else {
-      res.push(item);
-    }
-  }
-  return res;
-  //return (collection + '').split(',').map(x => Number(x));
+  return collection.flat();
 }
 
 module.exports = double_to_one;
--- main/flaten/double_dimensional_array_to_one_2.js
+++ main/flaten/double_dimensional_array_to_one_2.js
@@ -1,15 +1,8 @@
 "use strict";
 
 function double_to_one(collection) {
-  let res = [];
-  for (let item of collection) {
-    for (let itemIn of item) {
-      if (-1 === res.indexOf(itemIn)) {
-        res.push(itemIn);
-      }
-    }
-  }
-  return res;
+  let res = new Set(collection.flat());
+  return [...res];
 }
 
 module.exports = double_to_one;
